e66eaa4f6be8e60e7793ec8ad2ee1d04f04c4a41
Chanel <chanelnalani@gmail.com>
2012-09-28 10:34:51 -0700

    Exercise 17

---
+++ ex17.rb
@@ -0,0 +1,11 @@
+from_file, to_file = ARGV
+script = $0
+
+puts "Copying from #{from_file} to #{to_file}"
+
+input = File.open(from_file)
+indata = input.read()
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exists? to_file}"
